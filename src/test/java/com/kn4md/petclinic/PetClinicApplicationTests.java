8001abf7a20cab604ac1c4756301a453e6e04f3f
dm4nk <dim.xx2011@yandex.ru>
2021-10-25 18:51:37 +0400

    Initial commit

---
+++ src/test/java/com/kn4md/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kn4md.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
