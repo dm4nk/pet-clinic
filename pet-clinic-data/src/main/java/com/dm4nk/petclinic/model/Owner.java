3d3411ff1496552847b9c5ccb22b7d29e5196d8b
dm4nk <dim.xx2011@yandex.ru>
2021-11-12 14:45:59 +0400

    added properties to owners. closes #28

--- pet-clinic-data/src/main/java/com/dm4nk/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/dm4nk/petclinic/model/Owner.java
@@ -1,13 +1,19 @@
 package com.dm4nk.petclinic.model;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.util.Set;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Owner extends Person {
 
-    private Set<Pet> pets;
+    String address;
+    String city;
+    String telephone;
+    Set<Pet> pets;
 }
