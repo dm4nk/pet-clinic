30781c6efa29102cb3ec99403f0d6ccb80e8e7fc
dm4nk <dim.xx2011@yandex.ru>
2021-11-12 15:03:48 +0400

    Added PetType to bootstrap. closes#32

--- pet-clinic-web/src/main/java/com/dm4nk/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/dm4nk/petclinic/bootstrap/DataLoader.java
@@ -1,25 +1,40 @@
 package com.dm4nk.petclinic.bootstrap;
 
 import com.dm4nk.petclinic.model.Owner;
+import com.dm4nk.petclinic.model.PetType;
 import com.dm4nk.petclinic.model.Vet;
 import com.dm4nk.petclinic.services.OwnerService;
+import com.dm4nk.petclinic.services.PetTypeService;
 import com.dm4nk.petclinic.services.VetService;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class DataLoader implements CommandLineRunner {
 
-    private final OwnerService ownerService;
-    private final VetService vetService;
+    OwnerService ownerService;
+    VetService vetService;
+    PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -29,7 +44,6 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
-
         vetService.save(vet1);
 
         System.out.println("Loaded data");
