2731cdb30bc0bf73c2d2e229cf7c532a409eb9ba
dm4nk <dim.xx2011@yandex.ru>
2021-11-19 18:15:57 +0400

    added jpa service for Pet. closes #43

---
+++ pet-clinic-data/src/main/java/com/dm4nk/petclinic/services/jpa/PetJPAService.java
@@ -0,0 +1,50 @@
+package com.dm4nk.petclinic.services.jpa;
+
+import com.dm4nk.petclinic.model.Pet;
+import com.dm4nk.petclinic.repositories.PetRepository;
+import com.dm4nk.petclinic.services.PetService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class PetJPAService implements PetService {
+
+    PetRepository petRepository;
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> petSet = new HashSet<>();
+        petRepository.findAll().forEach(petSet::add);
+        return petSet;
+    }
+
+    @Override
+    public Pet findByID(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        petRepository.deleteById(id);
+    }
+}
