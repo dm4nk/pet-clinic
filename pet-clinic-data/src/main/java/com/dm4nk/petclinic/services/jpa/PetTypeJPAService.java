1e34cae9b8764d663922063c6b5270f6e678ec2c
dm4nk <dim.xx2011@yandex.ru>
2021-11-19 18:11:49 +0400

    added jpa service for PetTypes. closes #42

---
+++ pet-clinic-data/src/main/java/com/dm4nk/petclinic/services/jpa/PetTypeJPAService.java
@@ -0,0 +1,50 @@
+package com.dm4nk.petclinic.services.jpa;
+
+import com.dm4nk.petclinic.model.PetType;
+import com.dm4nk.petclinic.repositories.PetTypeRepository;
+import com.dm4nk.petclinic.services.PetTypeService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class PetTypeJPAService implements PetTypeService {
+
+    PetTypeRepository petTypeRepository;
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypeSet = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypeSet::add);
+        return petTypeSet;
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
