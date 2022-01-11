104792ac051db8943b759930a2ce1519015e4cda
dm4nk <dim.xx2011@yandex.ru>
2022-01-11 15:45:56 +0400

    Fixed find owners. closes #63

--- pet-clinic-data/src/main/java/com/dm4nk/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dm4nk/petclinic/repositories/OwnerRepository.java
@@ -1,6 +1,7 @@
 package com.dm4nk.petclinic.repositories;
 
 import com.dm4nk.petclinic.model.Owner;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
 
+
+    @Query("select o from Owner o where o.lastName like %?1%")
     List<Owner> findAllByLastNameLike(String lastName);
 }
