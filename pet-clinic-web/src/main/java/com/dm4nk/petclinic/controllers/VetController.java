4352651be08ef816b97450cc1d7d25abb64d1806
dm4nk <dim.xx2011@yandex.ru>
2022-01-13 21:28:50 +0400

    Implemented Vets Json Endpoint. closes #66

--- pet-clinic-web/src/main/java/com/dm4nk/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/dm4nk/petclinic/controllers/VetController.java
@@ -1,11 +1,16 @@
 package com.dm4nk.petclinic.controllers;
 
+import com.dm4nk.petclinic.model.Vet;
 import com.dm4nk.petclinic.services.VetService;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -23,5 +28,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping({"/api/vets", "/api/vets.html"})
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
